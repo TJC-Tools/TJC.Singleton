a752b930a7f7adc241706f062c463f5a6c0f5f1e
TCarrol <tylercarrol@hotmail.com>
2024-05-31 14:40:57 -0400

    Create Constructor Tests

--- TJC.Singleton.Tests/GlobalUsings.cs
+++ TJC.Singleton.Tests/GlobalUsings.cs
@@ -1,4 +1,7 @@
 global using System.Diagnostics;
+global using TJC.Singleton.Exceptions;
 global using TJC.Singleton.Tests.Helpers;
 global using TJC.Singleton.Tests.Interfaces;
+global using TJC.Singleton.Tests.Mocks.InvalidConstructors;
+global using TJC.Singleton.Tests.Mocks.NonThreadSafe;
 global using TJC.Singleton.Tests.Mocks.Valid;
---
+++ TJC.Singleton.Tests/Tests/Constructor/ConstructorTests.cs
@@ -0,0 +1,35 @@
+﻿namespace TJC.Singleton.Tests.Tests.Constructor;
+
+[TestClass]
+public class ConstructorTests
+{
+    [TestMethod]
+    public void PrivateConstructorIsInitialized()
+    {
+        Assert.IsNotNull(MockSingletonValid.Instance);
+    }
+
+    [TestMethod]
+    public void NoConstructorThrowsException()
+    {
+        Assert.ThrowsException<InvalidSingletonConstructorException>(() => MockSingletonNoConstructor.Instance);
+    }
+
+    [TestMethod]
+    public void PublicConstructorThrowsException()
+    {
+        Assert.ThrowsException<InvalidSingletonConstructorException>(() => MockSingletonPublicConstructor.Instance);
+    }
+
+    [TestMethod]
+    public void ProtectedConstructorWithParametersThrowsException()
+    {
+        Assert.ThrowsException<InvalidSingletonConstructorException>(() => MockSingletonProtectedConstructorWithParameters.Instance);
+    }
+
+    [TestMethod]
+    public void PrivateConstructorWithParametersThrowsException()
+    {
+        Assert.ThrowsException<InvalidSingletonConstructorException>(() => MockSingletonPrivateConstructorWithParameters.Instance);
+    }
+}
