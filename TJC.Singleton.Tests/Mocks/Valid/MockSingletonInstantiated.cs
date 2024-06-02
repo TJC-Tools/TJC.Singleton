eee9b2b916697e6999bf9d1df97a053505921f51
Tyler Carrol <tylercarrol@hotmail.com>
2024-06-02 11:59:12 -0400

    Create test for singleton factory, to ensure it properly instantiates the singletons

---
+++ TJC.Singleton.Tests/Mocks/Valid/MockSingletonInstantiated.cs
@@ -0,0 +1,10 @@
+﻿namespace TJC.Singleton.Tests.Mocks.Valid;
+
+internal class MockSingletonInstantiated : SingletonBaseClass<MockSingletonInstantiated>, IIdentifier
+{
+    private MockSingletonInstantiated() {}//=> IsInstantiated = true;
+
+    public Guid Id { get; } = Guid.NewGuid();
+
+    //public static bool IsInstantiated { get; private set; }
+}
