d17f2838119f5cf52477244f96f6e584e7b8fc2f
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-08 19:46:51 -0400

    🧹 chore: Comment Exceptions

--- TJC.Singleton/Exceptions/SingletonInitializationException.cs
+++ TJC.Singleton/Exceptions/SingletonInitializationException.cs
@@ -1,3 +1,7 @@
 ﻿namespace TJC.Singleton.Exceptions;
 
+/// <summary>
+/// This exception is thrown when a singleton fails to initialize.
+/// </summary>
+/// <param name="message"></param>
 public class SingletonInitializationException(string? message) : Exception(message);
