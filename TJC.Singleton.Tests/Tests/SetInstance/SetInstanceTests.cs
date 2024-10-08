de7e1f460bc018b8aa1d2daa75b5c89a90b8650e
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-08 19:41:13 -0400

    🚨 tests: Add Test for Set Instance

---
+++ TJC.Singleton.Tests/Tests/SetInstance/SetInstanceTests.cs
@@ -0,0 +1,29 @@
+﻿namespace TJC.Singleton.Tests.Tests.SetInstance;
+
+[TestClass]
+public class SetInstanceTests
+{
+    [TestMethod]
+    public void SetInstance_PredefinedTypes()
+    {
+        MockSingletonPreDefinedTypes.SetInstance(MockSingletonPreDefinedTypes.Default);
+
+        Assert.AreEqual("Setting1", MockSingletonPreDefinedTypes.Instance.Setting1);
+        Assert.AreEqual("Setting2", MockSingletonPreDefinedTypes.Instance.Setting2);
+
+        MockSingletonPreDefinedTypes.SetInstance(MockSingletonPreDefinedTypes.Empty);
+
+        Assert.AreEqual(string.Empty, MockSingletonPreDefinedTypes.Instance.Setting1);
+        Assert.AreEqual(string.Empty, MockSingletonPreDefinedTypes.Instance.Setting2);
+
+        MockSingletonPreDefinedTypes.SetInstance(MockSingletonPreDefinedTypes.Alphabet);
+
+        Assert.AreEqual("ABC", MockSingletonPreDefinedTypes.Instance.Setting1);
+        Assert.AreEqual("DEF", MockSingletonPreDefinedTypes.Instance.Setting2);
+
+        MockSingletonPreDefinedTypes.SetInstance(MockSingletonPreDefinedTypes.Numbers);
+
+        Assert.AreEqual("123", MockSingletonPreDefinedTypes.Instance.Setting1);
+        Assert.AreEqual("456", MockSingletonPreDefinedTypes.Instance.Setting2);
+    }
+}
