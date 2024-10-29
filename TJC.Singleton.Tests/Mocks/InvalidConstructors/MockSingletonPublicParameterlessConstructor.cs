2234f3ebb33118ca654b6f57f7dda1b96d0ec129
Tyler Carrol <tylercarrol@hotmail.com>
2024-10-28 21:44:32 -0400

    Rename Parameterless to ParameterLess

--- TJC.Singleton.Tests/Mocks/InvalidConstructors/MockSingletonPublicParameterlessConstructor.cs
+++ TJC.Singleton.Tests/Mocks/InvalidConstructors/MockSingletonPublicParameterlessConstructor.cs
@@ -1,10 +1,10 @@
 ﻿namespace TJC.Singleton.Tests.Mocks.InvalidConstructors;
 
-internal class MockSingletonPublicParameterlessConstructor
-    : SingletonBase<MockSingletonPublicParameterlessConstructor>,
+internal class MockSingletonPublicParameterLessConstructor
+    : SingletonBase<MockSingletonPublicParameterLessConstructor>,
         IIdentifier
 {
-    public MockSingletonPublicParameterlessConstructor() { }
+    public MockSingletonPublicParameterLessConstructor() { }
 
     public Guid Id { get; } = Guid.NewGuid();
 }
--- TJC.Singleton.Tests/Tests/Constructor/ConstructorTests.cs
+++ TJC.Singleton.Tests/Tests/Constructor/ConstructorTests.cs
@@ -21,7 +21,7 @@ public class ConstructorTests
     public void PublicParameterlessConstructorThrowsException()
     {
         Assert.ThrowsException<InvalidSingletonConstructorException>(
-            () => MockSingletonPublicParameterlessConstructor.Instance
+            () => MockSingletonPublicParameterLessConstructor.Instance
         );
     }
 
