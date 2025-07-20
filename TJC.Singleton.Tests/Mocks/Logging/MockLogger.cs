25c260343ce555fc5affbcd06e612ebb0f0e89bc
TylerCarrol <25536704+TylerCarrol@users.noreply.github.com>
2025-07-20 18:49:19 +0000

    🧹 chore: [MegaLinter] Apply [1] automatic fixes

--- TJC.Singleton.Tests/Mocks/Logging/MockLogger.cs
+++ TJC.Singleton.Tests/Mocks/Logging/MockLogger.cs
@@ -7,12 +7,17 @@ internal class MockLogger : ILogger
     public static MockLogger Default => new();
 
     public IDisposable? BeginScope<TState>(TState state)
-        where TState : notnull
-        => default!;
+        where TState : notnull => default!;
 
     public bool IsEnabled(LogLevel logLevel) => true;
 
-    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    public void Log<TState>(
+        LogLevel logLevel,
+        EventId eventId,
+        TState state,
+        Exception? exception,
+        Func<TState, Exception?, string> formatter
+    )
     {
         Trace.WriteLine($"[{logLevel}:{eventId}] {formatter(state, exception)}");
     }
